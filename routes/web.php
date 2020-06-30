c6d06b8f3cb06bb03752d8c0ed038cb5955985e3
Gregory <gregory09ruiz@gmail.com>
2020-06-29 21:29:34 -0400

    Variables_opcionales_en_url

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('user/{id}', function ($id) {
-    return 'Bienvenido User: ' . $id;
+Route::get('user/{name?}', function ($name=null) {
+    return 'Bienvenido: ' . $name;
 });
